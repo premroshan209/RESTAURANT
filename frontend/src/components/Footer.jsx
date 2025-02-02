7980eeb136f00a3889f4857565512c4df06fe0e4
Prem Roshan <premroshan209@gmail.com>
2025-02-02 13:35:42 +0530

    remaining-components-and-pages

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">BlackOut</div>
+          <div className="right">
+            <p> FVV6+893, Barrister Mullah Colony, Teliarganj, Prayagraj, Uttar Pradesh 211002</p>
+            <p>Open: 09:00 AM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>premroshan209@gmail.com</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Team Prem.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
